8dc92bb5c5d51a4655244cadaabd532fab1d7c06
kliminartem <klimin.artem@yandex.ru>
2019-09-25 02:48:44 +0400

    Для теста пробелы

--- easy.py
+++ easy.py
@@ -2,7 +2,7 @@ __author__ = 'Климин Артем Владимирович'
 
 # Задача 1
 
-print("Задача 1")
+print("Задача 1 ")
 userNumber = input("Введите число ")
 for eachItem in range(len(userNumber)):
     print(userNumber[eachItem])
@@ -10,7 +10,7 @@ print("Задача 1 завершена")
 
 # Задача 2
 
-print("Задача 2")
+print("Задача 2 ")
 a = input("Ведите значение переменной a ")
 b = input("Ведите значение переменной b ")
 a, b = b, a
@@ -20,7 +20,7 @@ print("Задача 2 завершена")
 
 # Задача 3
 
-print("Задача 3")
+print("Задача 3 ")
 userAge = int(input("Ведите ваш возраст "))
 if userAge >= 18:
     print("Извините, пользование данным ресурсом только с 18 лет")
